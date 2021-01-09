241a62962ca2e959ea5192900d193aa1387ce54d
Goldman Park <death2501@gmail.com>
2021-01-10 00:24:53 +0900

    21.01.10 edit

--- src/core.py
+++ src/core.py
@@ -2,14 +2,16 @@ import yfinance as yf
 import json
 import os
 
-tickers = ["^VIX", "^VXN", "DX-Y.NYB", "^IXIC", "^DJI", "^SPX"]
+tickers = []
+with open(os.path.join(os.getcwd(), "tickers.json"), "r") as jsonFile:
+    tickers = json.load(jsonFile)
 
-for x in tickers:
-    ticker = yf.Ticker(x)    
-    with open(os.path.join("info", x + ".json"), "w") as jsonFile:
+for x in tickers["tickerName"]:
+    ticker = yf.Ticker(x["name"])    
+    with open(os.path.join("info", x["name"] + ".json"), "w") as jsonFile:
         json.dump(ticker.info, jsonFile, indent=4)
 
     hist = ticker.history(start="2021-01-01", end="2021-01-08", interval="1d")
     histJson = json.loads(hist.to_json(orient="table"))
-    with open(os.path.join("history", x + ".json"), "w") as jsonFile:
+    with open(os.path.join("history", x["name"] + ".json"), "w") as jsonFile:
         json.dump(histJson, jsonFile, indent=4)
