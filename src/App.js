6fb42b2cae8c5970cf4aafc65506b94c58f9cb24
Goldman <death2501@gmail.com>
2021-01-13 19:53:55 +0900

    2021.01.13 edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import tickerJson from './jsonData/tickers.json'
-import { Chart } from './Chart';
+import Chart from './Chart';
 
 export default class App extends Component{
   constructor(props){
@@ -11,22 +11,37 @@ export default class App extends Component{
   }
 
   createCandlecharts = () => {
-    this.state.tickers.forEach(element => {
-      <Chart series={element['name']}/>
-    });
+    try {
+      this.state.tickers.forEach(x => {
+        var option = {
+          chart: { type: 'candlestick', height: 350 },
+          title: { text: x['name'], align: 'left' },
+          xaxis: { type: 'datetime' },
+          yaxis: { tooltip: { enabled: true } }
+        };
+        <Chart options={option}/>
+        console.log(x['name']);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }    
   }
 
   render(){
-    const tickerList = this.state.tickers
     return (
-       <div>
-         <h4 className="bg-primary text-white text-center p-1">
-           Talent
-         </h4>
-         <table>
+      <div>
+        <h4 className="bg-primary text-white text-center p-1">
+          Talent
+        </h4>
+        <table>
+          <thead>
+            <tr>
+              <th>ITEMS</th>
+            </tr>
+          </thead>
           <tbody>{ this.createCandlecharts() }</tbody>
-         </table>
-       </div>
+        </table>
+      </div>
     )
   }
 }
--- src/Chart.js
+++ src/Chart.js
@@ -1,41 +1,49 @@
 import React, { Component } from 'react';
-import ReactApexChart from 'apexcharts'
+import ReactApexChart from 'apexcharts';
 
-export class Chart extends Component{
+class Chart extends Component{
   constructor(props){
     super(props);
     this.state = {
-      options: {
-        chart: {
-          type: 'candlestick',
-          height: 350
-        },
-        title: {
-          text: 'CandleStick Chart',
-          align: 'left'
-        },
-        xaxis: {
-          type: 'datetime'
-        },
-        yaxis: {
-          tooltip: {
-            enabled: true
+      series: [{
+        data: [{
+            x: new Date(1538778600000),
+            y: [6629.81, 6650.5, 6623.04, 6633.33]
+          },
+          {
+            x: new Date(1538780400000),
+            y: [6632.01, 6643.59, 6620, 6630.11]
+          },
+          {
+            x: new Date(1538782200000),
+            y: [6630.71, 6648.95, 6623.34, 6635.65]
+          },
+          {
+            x: new Date(1538784000000),
+            y: [6635.65, 6651, 6629.67, 6638.24]
+          },
+          {
+            x: new Date(1538785800000),
+            y: [6638.24, 6640, 6620, 6624.47]
           }
-        }
-      }
+        ]
+      }]
     }
   }
 
-  render (){
-      return (
-          <tr>
-            <td>
-            <ReactApexChart options={this.state.options} 
-                            series={this.state.series} 
-                            type="candlestick" 
-                            height={350} />
-            </td>            
-          </tr>
-      )
+  render = () => {
+    console.log("xxx");
+    return (
+      <tr>
+        <td>
+          <ReactApexChart options={this.state.options} 
+                          series={this.state.series} 
+                          type="candlestick" 
+                          height={350} />
+        </td>            
+      </tr>
+    )
   }
 }
+
+export default Chart;
