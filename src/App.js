876052760440279a02f5c560e938f23ae146ea8d
Goldman <death2501@gmail.com>
2021-01-28 22:34:29 +0900

    21.01.28 edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import dayjs from 'dayjs';
-import { CandleStickChart } from './CandleStickChart';
-import { option as candleStickOption } from './candleStickOption.json'
+import { Header } from './components/Header';
+import { CandleStickChart } from './components/CandleStickChart';
+import { option as candleStickOption } from './chartTemplate/candleStickOption.json'
 
 export default class App extends Component{
   constructor(props){
     super(props);
-    var today = new Date();
-    var ago = new Date(today.getTime() - (30 * 24 * 60 * 60 * 1000));
     this.state = {
       tickers : [], // list of tickers
       details : [], // list of security details
-      startDate : ago.toISOString().substr(0,10),
-      endDate : today.toISOString().substr(0,10)
     }
-    this.updateStartDate = this.updateStartDate.bind(this);
-    this.updateEndDate = this.updateEndDate.bind(this);
-    this.submitDateRange = this.submitDateRange.bind(this);
+    this.headerSubmitCallback = this.headerSubmitCallback.bind(this);
   }
 
   componentDidMount(){
@@ -44,27 +39,11 @@ export default class App extends Component{
     });
   }
 
-  compareDate(date1, date2){
-    let date = dayjs(date2)
-    return date.diff(date1, 'day'); // date2 - date1
-  }
-
-  updateStartDate = (event) => 
-    this.setState({startDate : event.target.value});
-
-  updateEndDate = (event) =>
-    this.setState({endDate : event.target.value});
-
-  submitDateRange = (event) => {
-    event.preventDefault();
-    if(this.compareDate(this.state.startDate, this.state.endDate) < 0)
-      alert("date error");
-    else if(this.state.tickers != null){
-      this.setState({details : []});
-      this.state.tickers.forEach(x => this.getJsonData(x.name));
-    }      
-    else
-      console.log("no tickers!");
+  headerSubmitCallback = (_startDate, _endDate) => {
+    this.setState({details : []});
+    this.setState({startDate : _startDate});
+    this.setState({endDate : _endDate});
+    this.state.tickers.forEach(x => this.getJsonData(x.name));
   }
 
   createCandlecharts = () => {
@@ -84,38 +63,7 @@ export default class App extends Component{
   render(){
     return (
       <div>
-        <div class="main-header">          
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <span class="navbar-brand mb-0 h1">Talent</span>
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-              <li class="nav-item">
-                <a class="nav-link font-weight-bold" href="#"> Market Index </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link font-weight-bold" href="#"> Currency </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link font-weight-bold dropdown-toggle" href="#" id="navbarDropdown" role="button" 
-                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Future
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item">Gold</a>
-                  <a class="dropdown-item">Copper</a>
-                  <a class="dropdown-item">Oil</a>
-                </div>
-              </li>
-            </ul>
-            <form class="form-inline" onSubmit={ this.submitDateRange }>
-              <input type="date" class="form-control m-sm-1 p-1" value={this.state.startDate} onChange={this.updateStartDate}/>
-              <span class="navbar-text mx-2"> ~ </span>
-              <input type="date" class="form-control m-sm-1 p-1" value={this.state.endDate} onChange={this.updateEndDate}/>
-              <span> </span>
-              <input type="submit" class="btn btn-secondary font-weight-bold m-1" value="Send Request"/>
-            </form>
-          </nav>          
-        </div>
-        
+        <Header callbackSubmit={ this.headerSubmitCallback }/>
         <div class="square-FlexGrid">
           { this.createCandlecharts() }
         </div>
---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import dayjs from 'dayjs';
+
+export class Header extends React.Component{
+  constructor(props){
+    super(props);
+    var today = new Date();
+    var ago = new Date(today.getTime() - (30 * 24 * 60 * 60 * 1000));
+    this.state = {
+      startDate : ago.toISOString().substr(0,10),
+      endDate :  today.toISOString().substr(0,10)
+    }
+    this.updateStartDate = this.updateStartDate.bind(this);
+    this.updateEndDate = this.updateEndDate.bind(this);
+    this.submitDateRange = this.submitDateRange.bind(this);
+  }
+  
+  updateStartDate = (event) => 
+    this.setState({startDate : event.target.value});
+
+  updateEndDate = (event) =>
+    this.setState({endDate : event.target.value});
+
+  submitDateRange = (event) => {
+    event.preventDefault();
+    let date = dayjs(this.state.endDate);
+    let result = date.diff(this.state.startDate, 'day');
+    if(result < 0)
+      alert("date error");
+    else{
+      let _startDate = this.state.startDate;
+      let _endDate = this.state.endDate;
+      this.props.callbackSubmit(_startDate, _endDate);
+    }      
+  }
+
+  render() {
+    return (
+      <div class="main-header">          
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <span class="navbar-brand mb-0 h1">Talent</span>
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link font-weight-bold" href="#"> Market Index </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link font-weight-bold" href="#"> Currency </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link font-weight-bold dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Future
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item">Gold</a>
+                <a class="dropdown-item">Copper</a>
+                <a class="dropdown-item">Oil</a>
+              </div>
+            </li>
+          </ul>
+          <form class="form-inline" onSubmit={ this.submitDateRange }>
+            <input type="date" class="form-control m-sm-1 p-1" value={this.state.startDate} onChange={this.updateStartDate}/>
+            <span class="navbar-text mx-2"> ~ </span>
+            <input type="date" class="form-control m-sm-1 p-1" value={this.state.endDate} onChange={this.updateEndDate}/>
+            <span> </span>
+            <input type="submit" class="btn btn-secondary font-weight-bold m-1" value="Send Request"/>
+          </form>
+        </nav>
+      </div>
+    )
+  }
+}
